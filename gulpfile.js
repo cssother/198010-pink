a49caf7adf69d11f31a6ece66b1b3cc3e508fb47
Serhei Trubkin <trubkinsa@gmail.com>
2016-12-25 13:30:15 +0300

    Дописаны скрипты для build, svg иконки переместил в папку icons для создания спрайта

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,8 @@ var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
 var svgstore = require("gulp-svgstore");
 var svgmin = require("gulp-svgmin");
+var run = require("run-sequence");
+var del = require("del");
 
 gulp.task("style", function() {
   gulp.src("less/style.less")
@@ -21,53 +23,68 @@ gulp.task("style", function() {
       autoprefixer({browsers: [
         "last 2 versions"
       ]})
-    ]))
-    .pipe(gulp.dest("css"))
-    .pipe(server.stream());
+    mqpacker({ sort: true })
+  ]))
+  .pipe(gulp.dest("build/css"))
+  .pipe(minify())
+  .pipe(rename("style.min.css"))
+  .pipe(gulp.dest("build/css"));
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("serve", function() {
   server.init({
-    server: ".",
+    server: "build",
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
-
   gulp.watch("less/**/*.less", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
 });
 
-gulp.task("style", function() {
-  gulp.src("less/style.less")
-  .pipe(plumber())
-  .pipe(less())
-  .pipe(postcss([
-    autoprefixer({browsers: [ … ]}),
-    mqpacker({ … })
-    ]))
-  .pipe(gulp.dest("css"))
-  .pipe(minify())
-  .pipe(rename("style.min.css"))
-  .pipe(gulp.dest("css"));
-});
-
 gulp.task("images", function() {
-  return gulp.src("img / /.{png,jpg,gif}")
+  return gulp.src("build/img/**/*.{png,jpg,gif}")
   .pipe(imagemin([
     imagemin.optipng({optimizationLevel: 3}),
     imagemin.jpegtran({progressive: true})
   ]))
-  .pipe(gulp.dest("img"));
+  .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("symbols", function() {
-  return gulp.src("img/icons /.svg")
+  return gulp.src("build/img/icons/*.svg")
   .pipe(svgmin())
   .pipe(svgstore({
     inlineSvg: true
   }))
   .pipe(rename("symbols.svg"))
-  .pipe(gulp.dest("img"));
+  .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+    "img/**",
+    "js/**",
+    "*.html"
+    ], {
+      base: "."
+    })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("build", function(fn) {
+  run(
+    "clean",
+    "copy",
+    "style",
+    "images",
+    "symbols",
+    fn
+  );
 });
