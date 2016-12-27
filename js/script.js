7e3faf8affaa6837e19ba1544d7453d57c521817
Serhei Trubkin <trubkinsa@gmail.com>
2016-12-27 13:32:57 +0300

    Исправление замечаний финальной проверки

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 var button = document.querySelector(".header__button");
 var menu = document.querySelector(".main-nav");
 var topHeader = document.querySelector(".header-top");
+var navMain = document.querySelector(".main-nav");
+var mapWrap = document.querySelector(".map__wrapper");
+
+topHeader.classList.remove("header-top--no-js");
+navMain.classList.remove("main-nav--no-js");
+mapWrap.classList.remove("map__wrapper--no-js");
 
 button.addEventListener("click", function(event) {
   event.preventDefault();
