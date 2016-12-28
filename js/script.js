b7027cc2384ca3d3ee0d21fd76b5770b3f333a31
Serhei Trubkin <trubkinsa@gmail.com>
2016-12-28 19:11:39 +0300

    исправление замечаний финальной проверки. Модальные окна, логотип оп стайлгайду, частичная подгонка размеров по макету на разных браузерах

--- js/script.js
+++ js/script.js
@@ -4,13 +4,17 @@ var topHeader = document.querySelector(".header-top");
 var navMain = document.querySelector(".main-nav");
 var mapWrap = document.querySelector(".map__wrapper");
 
-topHeader.classList.remove("header-top--no-js");
-navMain.classList.remove("main-nav--no-js");
-mapWrap.classList.remove("map__wrapper--no-js");
+
 
 button.addEventListener("click", function(event) {
   event.preventDefault();
   menu.classList.toggle("main-nav--closed");
   button.classList.toggle("header__button--close");
   topHeader.classList.toggle("header-top--opened");
+
+
 })
+
+topHeader.classList.remove("header-top--no-js");
+navMain.classList.remove("main-nav--no-js");
+mapWrap.classList.remove("map__wrapper--no-js");
