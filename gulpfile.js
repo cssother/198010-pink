2d3910eea3b26441efc69f8d2ba401ca0670eb51
Sergey Trubkin <trubkinsa@gmail.com>
2017-01-21 19:39:35 +0300

    Перемещение исходных файлов проекта в папку src

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,31 @@
 "use strict";
 
+
 var gulp = require("gulp");
-var less = require("gulp-less");
 var plumber = require("gulp-plumber");
+var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
 var mqpacker = require("css-mqpacker");
 var minify = require("gulp-csso");
-var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
+var rename = require("gulp-rename");
 var svgstore = require("gulp-svgstore");
 var svgmin = require("gulp-svgmin");
+var server = require("browser-sync").create();
 var run = require("run-sequence");
+var ghPages = require("gulp-gh-pages");
+var uglify = require("gulp-uglify");
 var del = require("del");
 
+
 gulp.task("style", function() {
-  gulp.src("less/style.less")
+  gulp.src("src/less/style.less")
     .pipe(plumber())
     .pipe(less())
-    .pipe(postcss([
-      autoprefixer({browsers: [
-        "last 2 versions", "IE 11"
-      ]})
-      // mqpacker ({ sort: true })
+    .pipe(postcss([                                                 // делаем постпроцессинг
+      autoprefixer({ browsers: ["last 2 version", "IE 11"] }),     // автопрефиксирование
+      mqpacker({ sort: true })                                     // объединение медиавыражений
     ]))
     .pipe(gulp.dest("build/css"))
     .pipe(minify())
@@ -32,11 +34,23 @@ gulp.task("style", function() {
     .pipe(server.stream());
 });
 
+gulp.task("minjs", function() {
+  gulp.src("src/js/*.js")
+    .pipe(gulp.dest("build/js/"))
+    .pipe(uglify())
+    .pipe(gulp.dest("build/js/minjs"));
+});
+
+gulp.task("html", function() {
+  gulp.src("src/*html")
+    .pipe(gulp.dest("build"));
+});
+
 gulp.task("images", function() {
-  return gulp.src("build/img/**/*.{png, jpg, gif}")
-  .pipe(imagemin([
-    imagemin.optipng({optimizationLevel: 3}),
-    imagemin.jpegtran({progressive: true})
+  return gulp.src("build/img/**/*.{jpg, png, gif}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true})
   ]))
   .pipe(gulp.dest("build/img"));
 });
@@ -49,22 +63,27 @@ gulp.task("symbols", function() {
   }))
   .pipe(rename("symbols.svg"))
   .pipe(gulp.dest("build/img"));
-});
+})
 
 gulp.task("copy", function() {
   return gulp.src([
-    "fonts/**/*.{woff,woff2}",
-    "img/**",
-    "js/**",
-    "*.html"
-    ], {
-      base: "."
-    })
+    "src/fonts/**/*.{woff,woff2}",
+    "src/img/**",
+    "src/js/**",
+    "src/*.html"
+  ], {
+    base: "src/"
+  })
   .pipe(gulp.dest("build"));
 });
 
 gulp.task("clean", function() {
-  return del("build");
+  return del("build")
+});
+
+gulp.task("deploy", function() {
+  return gulp.src("./build/**/*")
+    .pipe(ghPages());
 });
 
 gulp.task("build", function(fn) {
@@ -72,6 +91,7 @@ gulp.task("build", function(fn) {
     "clean",
     "copy",
     "style",
+    "minjs",
     "images",
     "symbols",
     fn
@@ -83,10 +103,10 @@ gulp.task("serve", function() {
     server: "build",
     notify: false,
     open: true,
-    cors: true,
     ui: false
   });
 
-  gulp.watch("less/**/*.less", ["style"]);
-  gulp.watch("*.html").on("change", server.reload);
+  gulp.watch("src/less/**/*.less", ["style"]);
+  gulp.watch("src/js/**/*.js", ["minjs"]);
+  gulp.watch("src/*.html", ["html"]).on("change", server.reload);
 });
