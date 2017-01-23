5859dbf89cedb23f45bdb3a8e40fa774d3932049
Sergey Trubkin <trubkinsa@gmail.com>
2017-01-23 14:52:35 +0300

    Initial build subtree commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var button = document.querySelector(".header__button");
+var menu = document.querySelector(".main-nav");
+var topHeader = document.querySelector(".header-top");
+var navMain = document.querySelector(".main-nav");
+var mapWrap = document.querySelector(".map__wrapper");
+
+
+
+button.addEventListener("click", function(event) {
+  event.preventDefault();
+  menu.classList.toggle("main-nav--closed");
+  button.classList.toggle("header__button--close");
+  topHeader.classList.toggle("header-top--opened");
+
+
+})
+
+topHeader.classList.remove("header-top--no-js");
+navMain.classList.remove("main-nav--no-js");
+mapWrap.classList.remove("map__wrapper--no-js");
